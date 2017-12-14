f1dc1c4ef84a87728348e755dd793f1295128aa8
Damjan-PC\Damjan Miloshevski <d.miloshevski@gmail.com>
2017-12-14 23:05:05 +0100

    changed opacity of backgrounds

--- app/src/main/java/com/iamdamjanmiloshevski/instagramclone/activity/LoginActivity.java
+++ app/src/main/java/com/iamdamjanmiloshevski/instagramclone/activity/LoginActivity.java
@@ -15,7 +15,6 @@ import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
